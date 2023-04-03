aa05d6fbc5300a9b5717ce1d6c0f8b1dd6b8fe57
ROBILLIARD Grégory <gregory.robilliard@icloud.com>
2023-04-03 14:43:16 +0200

    Part 3 : 13

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,5 +1,63 @@
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
+import { useParams, Link } from "react-router-dom";
+
+const CATEGORY = gql`
+  query Category($id: ID!) {
+    category(id: $id) {
+      data {
+        id
+        attributes {
+          name
+          reviews {
+            data {
+              id
+              attributes {
+                title
+                body
+                rating
+                categories {
+                  data {
+                    id
+                    attributes {
+                      name
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default function Category() {
-  return <div>Category</div>;
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(CATEGORY, {
+    variables: { id: id },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  console.log(data);
+
+  return (
+    <div>
+      <h2>{data.category.data.attributes.name}</h2>
+      {data.category.data.attributes.reviews.data.map((review) => (
+        <div key={review.id} className="review-card">
+          <div className="rating">{review.attributes.rating}</div>
+          <h2>{review.attributes.title}</h2>
+          {review.attributes.categories.data.map((category) => (
+            <small key={category.id}>{category.attributes.name}</small>
+          ))}
+          <p>{review.attributes.body.substring(0, 200)}...</p>
+          <Link to={`/reviews/${review.id}`}>Read more</Link>
+        </div>
+      ))}
+    </div>
+  );
 }
